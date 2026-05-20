75d44ef21ec3b667f8b0ea8147a2d595e83c58f8
Hallan Miranda <hallanlmm@gmail.com>
2026-05-20 08:44:34 -0300

    teste

--- chamber/script/members.js
+++ chamber/script/members.js
@@ -1,44 +1,41 @@
-const cards = document.querySelector('#cards')
-const exemple = [
-    {
-        "name": "Hallan Miranda",
-        "tag": "volunter",
-        "photoUrl": "https://hallan-miranda.github.io/WDD231-Let-s-Bora/images/profile.jpg",
-        "email": "teste@londrinacc.org",
-        "phone": "43 2345-6789",
-        "url": "hallan-miranda.com"
-    }
-]
+const url = "https://hallan-miranda.github.io/WDD231-Let-s-Bora/chamber/members.json";
+const memberCards = document.querySelector('#cards');
 
-function creatCard(card){
+async function getMembers(){
+    const response = await fetch(url);
+    const data = await response.json();
 
-    let div = document.createElement('div');
-    let h3 = document.createElement('h3');
-    let p1 = document.createElement('p');
-    let img = document.createElement('img');
-    let span = document.createElement('span');
+    creatCard(data.memeber);
+};
 
-    div.classList.add('card')
+function creatCard(cards){
+    cards.forEach(card => {
+        let div = document.createElement('div');
+        let h3 = document.createElement('h3');
+        let p1 = document.createElement('p');
+        let img = document.createElement('img');
+        let span = document.createElement('span');
+    
+        div.classList.add('card')
+    
+        h3.textContent = `${card.name}`;
+        p1.textContent = `${card.tag}`;
+        span.textContent = `E-mail: ${card.email}\nPhone: ${card.phone}\nSite: ${card.url}`;
+    
+        img.setAttribute("src", card.photoUrl);
+        img.setAttribute("alt", `image of ${card.name}`);
+        img.setAttribute("loading", "lazy");
+        img.setAttribute("width", "40px");
+        img.setAttribute("heigth", "40px");
+    
+        div.appendChild(h3);
+        div.appendChild(p1);
+        div.appendChild(img);
+        div.appendChild(span);
+        memberCards.appendChild(div);
+    });
 
-    h3.textContent = `${card.name}`;
-    p1.textContent = `${card.tag}`;
-    span.textContent = `E-mail: ${card.email}\nPhone: ${card.phone}\nSite: ${card.url}`;
+};
 
-    img.setAttribute("src", card.photoUrl);
-    img.setAttribute("alt", `image of ${card.name}`);
-    img.setAttribute("loading", "lazy");
-    img.setAttribute("width", "40px");
-    img.setAttribute("heigth", "40px");
-
-    div.appendChild(h3);
-    div.appendChild(p1);
-    div.appendChild(img);
-    div.appendChild(span);
-    cards.appendChild(div);
-
-}
-
-exemple.forEach(card => {
-    creatCard(card)
-});
+getMembers();
 
